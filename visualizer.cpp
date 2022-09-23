81e52a5895da29606fca4c43b1e3012f9d0d19f5
Philipp Waehnert <philipp.waehnert@posteo.de>
2022-09-23 15:01:51 +0200

    Uses approximated sawtooth movement

--- visualizer.cpp
+++ visualizer.cpp
@@ -3,6 +3,15 @@
 #include <functional>
 #include <stdexcept>
 
+double sawtooth(double t) {
+    double scale = 2.0 / M_PI;
+    double sum = sin(M_PI * t);
+    sum -= sin(2 * M_PI * t) / 2.0;
+    sum += sin(3 * M_PI * t) / 3.0;
+    sum -= sin(4 * M_PI * t) / 4.0;
+    return scale * sum;
+}
+
 double psi(double t) {
     if (t <= 0) {
         return 0;
@@ -27,9 +36,10 @@ double hat(double t) {
 
 class Rectangle {
 public:
-    explicit Rectangle(SDL_Renderer *renderer)
+    explicit Rectangle(SDL_Renderer *renderer, double period)
       : renderer(renderer),
-        texture(SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 100, 100))
+        texture(SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 100, 100)),
+        period(period)
     {
         if (texture == nullptr) {
             throw std::runtime_error("Can't create texture for rectangle");
@@ -47,8 +57,8 @@ public:
         SDL_RenderClear(renderer);
         SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
         SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-        const double width = 10.0 * sin(2 * M_PI * time / 2000) + 80.0;
-        const double height = -5.0 * sin(2 * M_PI * time / 2000) + 40.0;
+        const double width = 10.0 * sin(2 * M_PI * time / period) + 80.0;
+        const double height = -5.0 * sin(2 * M_PI * time / period) + 40.0;
         SDL_Rect r = { (100 - width) / 2, (100 - height) / 2, width, height };
         SDL_RenderFillRect(renderer, &r);
         SDL_SetRenderTarget(renderer, nullptr);
@@ -65,6 +75,7 @@ public:
 private:
     SDL_Renderer *renderer;
     SDL_Texture *texture;
+    double period;
 };
 
 int main(int argc, char *argv[]) {
@@ -85,23 +96,32 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    Rectangle rectangle(renderer);
+    Rectangle rectangle(renderer, 2000);
 
     SDL_Event e;
     bool quit = false;
-    uint32_t time = 0;
+    const uint32_t initial = SDL_GetTicks();
     while (!quit) {
-        time = SDL_GetTicks();
+        const uint32_t now = SDL_GetTicks();
+        const uint32_t time = now - initial;
         rectangle.render(time);
         SDL_SetRenderDrawColor(renderer, 96, 128, 255, 255);
         SDL_RenderClear(renderer);
-        const double angle = (70 * 2 * M_PI / 10000) * time + 90 * sin(2 * M_PI * time / 10000);
+        const double angle = 180.0 * sawtooth(time / 5000.0);
+        //const double angle = (70 * 2 * M_PI / 10000) * time + 90 * sin(2 * M_PI * time / 10000);
         rectangle.blit_to(nullptr, 270, 190, angle);
         while (SDL_PollEvent(&e)) {
             switch (e.type) {
             case SDL_QUIT:
                 quit = true;
                 break;
+            case SDL_KEYDOWN:
+                SDL_KeyboardEvent keyEvent = e.key;
+                switch (keyEvent.keysym.scancode) {
+                case SDL_SCANCODE_ESCAPE:
+                    quit = true;
+                    break;
+                }
             }
         }
         SDL_RenderPresent(renderer);
