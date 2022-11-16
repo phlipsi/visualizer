70629ada1d27ae1cda1b5b75a466f9341b98aa22
Philipp Waehnert <philipp.waehnert@posteo.de>
2022-11-16 22:19:03 +0100

    Adds measure and parameter selection to plot script

--- plot.py
+++ plot.py
@@ -1,18 +1,38 @@
 from matplotlib.pyplot import plot, show, axis
 from csv import reader
 from sys import argv
+from argparse import ArgumentParser
 
 
 if __name__ == '__main__':
-    with open(argv[1], 'r') as f:
+    parser = ArgumentParser()
+    parser.add_argument('filename')
+    parser.add_argument('--measures')
+    parser.add_argument('--parameter', default='measure', action='store')
+    args = parser.parse_args()
+
+    start = None
+    end = None
+    if args.measures:
+        start, end = args.measures.split('-')
+        if start:
+            start = float(start)
+        if end:
+            end = float(end)
+
+    with open(args.filename, 'r') as f:
         lines = reader(f, delimiter=';')
-        next(lines)
+        columns = next(lines)
+        column = columns.index(args.parameter)
+        if not column:
+            exit(1)
         xs = []
         ys = []
         for line in lines:
-            xs.append(float(line[0]))
-            ys.append(float(line[1]))
-    #for i in range(2, 100):
-    #    print(xs[i], ys[i])
+            measure = float(line[0])
+            if (not start or measure >= start) and (not end or measure < end):
+                xs.append(measure)
+                ys.append(float(line[column]))
+
     plot(xs[2:], ys[2:])
     show()
